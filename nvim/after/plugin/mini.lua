e2ac2283b9fa5f31b10a4b5ace5d2564dce80cce
Kevin Gaston Mansilla <55965367+kevmansilla@users.noreply.github.com>
2023-03-09 17:46:03 -0300

    Update mini.lua

--- nvim/after/plugin/mini.lua
+++ nvim/after/plugin/mini.lua
@@ -5,20 +5,21 @@ require('mini.pairs').setup()
 require('mini.trailspace').setup()
 require('mini.surround').setup()
 require('mini.move').setup(
-  {
-    -- Module mappings. Use `''` (empty string) to disable one.
-    mappings = {
-      -- Move visual selection in Visual mode.
-      left = '<C-h>',
-      right = '<C-l>',
-      down = '<C-j>',
-      up = '<C-k>',
+    {
+        -- Module mappings. Use `''` (empty string) to disable one.
+        mappings = {
+            -- Move visual selection in Visual mode.
+            left = '<C-j>',
+            right = '<C-l>',
+            down = '<C-k>',
+            up = '<C-i>',
 
-      -- Move current line in Normal mode
-      line_left = '<C-h>',
-      line_right = '<C-l>',
-      line_down = '<C-j>',
-      line_up = '<C-k>',
-    },
-  }
+            -- Move current line in Normal mode
+            line_left = '<C-j>',
+            line_right = '<C-l>',
+            line_down = '<C-k>',
+            line_up = '<C-i>',
+        },
+    }
 )
+
