fe8d47494600d3da74de57ee650468472952f575
Kevin Gaston Mansilla <55965367+kevmansilla@users.noreply.github.com>
2023-03-09 17:35:35 -0300

    Update lualine.lua

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -2,12 +2,8 @@ require("lualine").setup({
   options = {
     theme = "auto",
     component_separators = '|',
-    section_separators = { left = '', right = '' },
   },
   sections = {
-    lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
-    },
     lualine_b = {
       {
         "filename",
@@ -17,14 +13,10 @@ require("lualine").setup({
       {
         "diagnostics",
         sources = { "nvim_diagnostic" },
-        separator = { right = '' },
         symbols = { error = "✘ ", warn = "▲ ", info = "I ", hint = "⚑ " },
       },
     },
     lualine_x = {},
     lualine_y = { 'filetype', 'progress' },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
-    },
   },
 })
