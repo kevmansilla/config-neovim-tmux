41feb77b7ac843bcd80ec5089350564c25908be2
Kevin Mansilla <kevin.mansilla@mi.unc.edu.ar>
2023-03-13 16:49:22 -0300

    refactor

--- install.sh
+++ install.sh
@@ -14,35 +14,37 @@ grayColour="\e[0;37m\033[1m"
 echo -e "\n${yellowColour}[*]${endColour} Verificando si están los programas necesarios."
 sleep 2
 
-echo -e "\n${yellowColour}[**]${endColour} Oh my bash, está instalado?"
-test -f ~/.oh-my-bash
-if [ "$(echo $?)" == "1" ]; then
-  echo -e "${greenColour}(Sí)${endColour}"
-else
-  echo -e "${redColour}(No)${endColour}\n"
-  echo -e "${yellowColour}[**]${endColour} Instalando Oh my bash"
-  bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)" 2> /dev/null
-fi; sleep 1
+function dependencies(){
+  tput civis
+  clear; dependencies=(tmux nvim)
 
-echo -e "\n${yellowColour}[**]${endColour} Tmux, está instalado?"
-test -f ~/.tmux
-if [ "$(echo $?)" == "1" ]; then
-  echo -e "${greenColour}(Sí)${endColour}"
-else
-  echo -e "${redColour}(No)${endColour}\n"
-  echo -e "${yellowColour}[**]${endColour} Instalando tmux"
-  pamac install tmux > /dev/null 2>&1
-fi; sleep 1
 
-echo -e "\n${yellowColour}[**]${endColour} Neovim, está instalado?"
-test -f ~/.config/nvim/
-if [ "$(echo $?)" == "1" ]; then
-  echo -e "${greenColour}(Sí)${endColour}"
-else
-  echo -e "${redColour}(X)${endColour}\n"
-  echo -e "${yellowColour}[**]${endColour} Instalando nvim"
-  sudo pacman -S neovim > /dev/null 2>&1
-fi; sleep 1
+  echo -ne "\n${yellowColour}[*]${endColour}${blueColour} Programa${endColour} Oh-my-bash -> "
+  test -f ~/.oh-my-bash
+  if [ "$(echo $?)" == "1" ]; then
+    echo -e "${greenColour}(V)${endColour}"
+  else
+    echo -e "${redColour}(X)${endColour}\n"
+    echo -e "${yellowColour}[**]${endColour} Instalando Oh my bash"
+    bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)" 2> /dev/null
+  fi; sleep 1
+
+  for program in "${dependencies[@]}"; do
+    echo -ne "\n${yellowColour}[*]${endColour}${blueColour} Programa${endColour} $program ->"
+    test -f /usr/bin/$program
+    if [ "$(echo $?)" == "0" ]; then
+      echo -e " ${greenColour}(V)${endColour}"
+    else
+      echo -e " ${redColour}(X)${endColour}\n"
+      echo -e "${yellowColour}[**]${endColour}${grayColour} Instalando ${endColour}${blueColour}$program${endColour}${yellowColour}...${endColour}"
+      sudo pacman -S $program
+    fi; sleep 1
+  done
+}
+
+# Main Function
+
+dependencies
 
 echo -e "\n${yellowColour}[*]${endColour} Configurando - Remomoviendo dotfiles existentes"
 echo -e "\n${yellowColour}[**]${endColour} Removiendo config de neo vim"
@@ -53,8 +55,12 @@ echo -e "\n${yellowColour}[**]${endColour} Remomoviendo config tmux"
 rm -rf ~/.vim ~/.vimrc ~/.zshrc ~/.tmux ~/.tmux.conf ~/.config/nvim 2> /dev/null
 
 echo -e "\n${yellowColour}[**]${endColour} Linkeando con archivos del repo"
-mkdir -p ~/.config/ ~/.config/nvim/ ~/.config/nvim/lua/ ~/.config/nvim/lua/kevmansilla/
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/nvim/* ~/.config/nvim/
-
+mkdir ~/.config/nvim/
+mkdir ~/.config/nvim/lua/
+mkdir ~/.config/nvim/lua/kevmansilla/
+mkdir ~/.config/nvim/after
+mkdir ~/.config/nvim/after/plugin/
+cp nvim/init.lua ~/.config/nvim/
+cp nvim/after/plugin/* ~/.config/nvim/after/plugin/
+cp nvim/lua/kevmansilla/* ~/.config/nvim/lua/kevmansilla/
 
